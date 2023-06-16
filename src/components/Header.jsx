9be7eb756e4c92915cc30744ac03d604520585c8
Salohiddin <narutouzumakikurama81@gmail.com>
2023-06-16 21:05:53 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Header = () => {
+    return  <header className='header'>
+    <img src='https://www.designevo.com/res/templates/thumb_small/white-bulb-and-green-book.webp'/>
+  </header >
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const Profile = () => {
+    return  <div className='content'>
+
+    <div>
+   <img src='https://cdn.wallpapersafari.com/43/52/rkRsp5.jpg'/>
+    </div>
+
+    <div>
+      ava + description
+    </div>
+
+    <div>
+      My posts
+
+      <div>
+        New post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+}
+export default Profile;
